2d0b4e503f155c078bd6c54f4a6818f198f7c784
hozayves <muhozayves2014@gmail.com>
2022-02-21 23:31:56 +0200

    Delete event listener for placeholder Delete function for placeholder keyup event Added getTodo function for fetching data for local storage Make changes todoElement function Store todo (task) in local storage Delete todo (task) in local storage

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,8 +7,9 @@ const todoInput = document.querySelector('.todo__input__area__text'),
       counter = document.querySelector('.todo__navs__counter__count');
 
 // EVENT LISTENERS
-    // Input handle placehodler
-    todoInput.addEventListener('keyup', (e) =>  placeholder(todoInput.value, todoPlaceholder));
+
+    // Get content from localstorage 
+    document.addEventListener('DOMContentLoaded', getTodo);
     
     // capture input and then add todo to the list
     todoInput.addEventListener('keypress', addTodo);
@@ -21,21 +22,25 @@ const todoInput = document.querySelector('.todo__input__area__text'),
 
 // FUNCTIONS
 
-// HIDE PLACEHOLDER WHEN USER START TYPING
-function placeholder(text, change){
-    if(text <= 0) {
-        change.style.display = "block";
-    } else {
-        change.style.display = "none";
-    }
-}
 // ADDING A TODO INPUT
 function addTodo(e) {
-    
     if(e.key === "Enter") {
         e.preventDefault();
-        // function call for create todo element
-        todoElement(todoInput);
+        saveTodoLocalStorage(todoInput.value); // calling local storage function
+        const{id, todo, status} = getTodoFromLocalStorage()[getTodoFromLocalStorage().length - 1];
+        todoElement(todo, id); // function call for create todo element
+    }
+}
+// ADDING A TODO TASK TO EXIST UI FROM LOCAL STORAGE
+function getTodo() {
+    // Assign fetched data to exist UI
+    if(getTodoFromLocalStorage() === undefined) {
+        console.log('No task.');
+    } else {
+        const local = getTodoFromLocalStorage();
+        local.forEach(({todo, id}) => {
+            todoElement(todo, id);
+        })
     }
 }
 // FUNCTION FOR MARKING TODO AS COMPLETE
@@ -48,6 +53,7 @@ function markComplete(e) {
         targetElement.parentNode.children[1].classList.toggle('line-complete');
     } else if (targetElement.classList[0] === "todo__lists__list__delete") { // Delete a task
         const parentNode = targetElement.parentNode;
+        deleteTodoFromLocalStorage(targetElement.id); // Remove task in local storage
         parentNode.remove();
     }
 }
@@ -103,7 +109,7 @@ function todoCounter() {
 todoCounter();
 
 // FUNCTION FOR CREATE TODO ELEMENTS
-function todoElement(InputText) {
+function todoElement(task, id) {
     const list = document.createElement('div'); // overall div
     list.classList.add("todo__lists__list");
     const list_check = document.createElement('div'); // mark complete icon
@@ -114,11 +120,12 @@ function todoElement(InputText) {
     const list_content = document.createElement('div'); // div for todo text
     list_content.classList.add('todo__lists__list__todo')
     const list_content_span = document.createElement('span');
-    list_content_span.textContent = InputText.value;
+    list_content_span.textContent = task; // Setting todo from local storage
     const list_close = document.createElement('img'); // delete todo in the list
     list_close.classList.add('todo__lists__list__delete');
     list_close.setAttribute('src', "./dist/images/icon-cross.svg");
     list_close.setAttribute('alt', 'close-icon');
+    list_close.setAttribute('id', id); // Setting unique id to the from local storage
 
     list_content.appendChild(list_content_span);
     list_check.appendChild(imgCheck);
@@ -126,5 +133,53 @@ function todoElement(InputText) {
     list.appendChild(list_content);
     list.appendChild(list_close);
     todoLists.prepend(list);
-    InputText.value = "";
+    todoInput.value = '';
+    
+}
+// =======================================  LOCAL STORAGE ===================================
+
+// SAVE TODOS IN LOCAL STORAGE
+function saveTodoLocalStorage(tasks) {
+    // check if the storage exists
+    const id = new Date();
+    let todos;
+    if(localStorage.getItem('tasks') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('tasks'));
+    }
+    todos.push({'id': id, 'todo': tasks, 'status' : ''});
+    localStorage.setItem('tasks', JSON.stringify(todos));
+} 
+// GET TODOS FROM LOCAL STORAGE
+function getTodoFromLocalStorage() {
+    // check if the storage exists
+    let todos;
+    if(localStorage.getItem('tasks') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('tasks'));
+    }
+    let tasks = [];
+    todos.forEach(task => {
+        tasks.push(task);
+    });
+    return tasks;
+}
+// DELETE FROM THE LOCAL STORAGE
+function deleteTodoFromLocalStorage(taskId) {
+    // check if the storage exists
+    let todos;
+    if(localStorage.getItem('tasks') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('tasks'));
+    }
+    todos.forEach(todo => {
+        if(todo.id === taskId) {
+            const index = todos.indexOf(todo);
+            todos.splice(index, 1); // Remove task in the local storage
+            localStorage.setItem('tasks', JSON.stringify(todos)); // Update localStorage
+        }
+    })
 }
